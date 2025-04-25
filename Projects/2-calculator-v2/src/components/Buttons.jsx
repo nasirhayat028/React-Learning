3fca786ca848c9aa267ab298f3cf111ab591ae91
Nasir Hayat <nasirhayat028@gmail.com>
2025-04-25 15:52:55 -0400

    2-calculator-v2

--- Projects/2-calculator-v2/src/components/Buttons.jsx
+++ Projects/2-calculator-v2/src/components/Buttons.jsx
@@ -1,9 +1,35 @@
 import Style from "./Buttons.module.css";
 
 function Buttons() {
+  let Buttons = [
+    "C",
+    "1",
+    "2",
+    "+",
+    "3",
+    "4",
+    "-",
+    "5",
+    "6",
+    "*",
+    "7",
+    "8",
+    "/",
+    "=",
+    "9",
+    "0",
+    ".",
+  ];
   return (
     <>
       <div className={Style.buttons}>
+        {Buttons.map((buttonsName) => (
+          <>
+            {" "}
+            <button className={Style.button}>{buttonsName}</button>
+          </>
+        ))}
+        {/* 
         <button className={Style.button}>C</button>
         <button className={Style.button}>1</button>
         <button className={Style.button}>2</button>
@@ -22,7 +48,7 @@ function Buttons() {
         <button className={Style.button}>=</button>
         <button className={Style.button}>9</button>
         <button className={Style.button}>0</button>
-        <button className={Style.button}>.</button>
+        <button className={Style.button}>.</button> */}
       </div>
     </>
   );
